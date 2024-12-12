ff7b86f607a33e02c56bc4821dacb861f649f838
Adam <87928802+DnA-IntRicate@users.noreply.github.com>
2024-12-12 13:01:07 +0200

    MIPS Calculator implement arithmetic operations

--- MIPS/Calculator/Calculator.asm
+++ MIPS/Calculator/Calculator.asm
@@ -7,18 +7,33 @@
     SYS_READ: .byte 8
     SYS_EXIT: .byte 10
 
+    OP_ADD: .word 1
+    OP_SUB: .word 2
+    OP_MUL: .word 3
+    OP_DIV: .word 4
+    OP_MOD: .word 5
+
     NEWLINE: .asciiz "\n"
     prompt1: .asciiz "Enter a number: "
     prompt2: .asciiz "Enter another number: "
+    prompt3: .asciiz "Enter the operation to use:\n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - Modulo\n"
+    outputStr: .asciiz "\nThe answer is: "
 
 .text
-    # Print a string with a trailing newline
+    # Print a string
     Print:
         # $a0 = the string to print
+        # $a1 = 1 if this string should have a trailing newline, otherwise 0
         lb $v0, SYS_WRITE
         syscall
-        la $a0, NEWLINE
-        syscall
+
+        beq $a1, 1, NewLine
+        jr $ra
+
+        NewLine:
+            la $a0, NEWLINE
+            syscall
+
         jr $ra
 
     # Print an integer with a trailing newline
@@ -26,9 +41,11 @@
         # $a0 = the integer to print
         lb $v0, SYS_WRITE_INT
         syscall
+
         lb $v0, SYS_WRITE
         la $a0, NEWLINE
         syscall
+
         jr $ra
 
     # Read integer input from the user
@@ -37,17 +54,11 @@
         # $t0 = return value
         lb $v0, SYS_WRITE
         syscall
+
         lb $v0, SYS_READ_INT
         syscall             # User-inputted integer should now be in $v0
-        move $t0, $v0
-        jr $ra
 
-    # Adds 2 integers together
-    Sum:
-        # $a0 = integer a
-        # $a1 = integer b
-        # $t0 = return value
-        add $t0, $a0, $a1
+        move $t0, $v0
         jr $ra
 
     main:
@@ -63,14 +74,68 @@
         jal InputInt
         sw $t0, 4($sp)
 
-        # Add the 2 numbers together
-        lw $a0, 0($sp)
-        lw $a1, 4($sp)
-        jal Sum
+        # Get the user's desired mathematical operation
+        la $a0, prompt3
+        jal InputInt
 
-        # Print the sum of the 2 numbers
-        move $a0, $t0
-        jal PrintInt
+        # If-else-if chain
+        lw $t1, OP_ADD
+        beq $t0, $t1, Add
+        lw $t1, OP_SUB
+        beq $t0, $t1, Sub
+        lw $t1, OP_MUL
+        beq $t0, $t1, Mul
+        lw $t1, OP_DIV
+        beq $t0, $t1, Div
+        lw $t1, OP_MOD
+        beq $t0, $t1, Mod
+
+        # Add the numbers
+        Add:
+            lw $t1, 0($sp)
+            lw $t2, 4($sp)
+            add $t0, $t1, $t2
+            j Output
+
+        # Subtract the one number from the other
+        Sub:
+            lw $t1, 0($sp)
+            lw $t2, 4($sp)
+            sub $t0, $t1, $t2
+            j Output
+
+        # Multiply the numbers
+        Mul:
+            lw $t1, 0($sp)
+            lw $t2, 4($sp)
+            mult $t1, $t2
+            mflo $t0
+            j Output
+
+        # Divide the one number by the other
+        Div:
+            lw $t1, 0($sp)
+            lw $t2, 4($sp)
+            div $t1, $t2
+            mflo $t0
+            j Output
+
+        # Divide the one number by the other and return the modulo
+        Mod:
+            lw $t1, 0($sp)
+            lw $t2, 4($sp)
+            div $t1, $t2
+            mfhi $t0
+            j Output
+
+        # Print the result
+        Output:
+            la $a0, outputStr
+            xor $a1, $a1, $a1
+            jal Print
+
+            move $a0, $t0
+            jal PrintInt
 
         addi $sp, $sp, 8    # Deallocate stack space
         lb $v0, SYS_EXIT
